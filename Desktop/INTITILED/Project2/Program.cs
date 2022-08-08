16b8a1b379dab60ed01331920db07f62d8ec1c86
Diana <diana.norva@yandex.ru>
2022-08-08 10:31:51 +0300

    First work

---
+++ Desktop/INTITILED/Project2/Program.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        // тип [] название массива
+        // int [] numbers = new int[4] {1,1,23,4};
+
+        // int[] nums1 = new int[] {1,23,4,5};
+        // int[] nims2 = new[] {1,23,4,5};
+        int[] nums3 = {1,2,3,45,43,2,234,1,412,4,21,214,12,4,214,12,34};
+        for(int i = 1; i < nums3.Length; i++)
+        {
+            nums3[i] = 0;
+            Console.WriteLine(nums3[i]);
+        }
+    }        
+}
